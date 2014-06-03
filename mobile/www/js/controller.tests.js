a56042da889847040f91486fcf09ba853d2c7697
Florian Cechi <florian.cechi@gmail.com>
2014-06-03 18:08:25 +0300

    da

--- mobile/www/js/controller.tests.js
+++ mobile/www/js/controller.tests.js
@@ -1,42 +1,39 @@
 angular.module('caffeina.controller.tests', [])
-    .controller('tests', ['$scope', 'leads',function ($scope,leads ) {
+
+    .controller('tests', ['$scope', 'leads', function ($scope, leads) {
 
         $scope.addLead = function () {
-
+            var anyDate = moment("2014-01-01").add('day', parseInt(Math.random() * 100)).format("YYYY-MM-DD");
             var lead = {
-                "date": "2014-07-20",
+                "date": anyDate,
                 "title": "Lead de incarcat prin servicii",
-                "contact": {"name": "Florian Cechi", "phone": "7829387232", "email": "asdada@gmail.com"}
-            }
+                "contact": {"name": "Florian Cechi", "phone": "7829387232", "email": "asdada@gmail.com", id: 1}
+            };
             leads.add(lead);
-        }
-
+        };
 
         $scope.updateLead = function () {
 
+            var anyDate = moment("2014-01-01").add('day', parseInt(Math.random() * 100)).format("YYYY-MM-DD")
+                , idLead = $scope.idLead
+                ;
             var lead = {
-                "date": "2015-05-01",
+                "date": anyDate,
                 "title": "Lead de modificat prin servicii",
                 "contact": {"name": "Florian Cechi", "phone": "7829387232", "email": "122323423@gmail.com"}
-            }
-            leads.update(lead, '-JO7GKm4-BIRsHI4ev9is');
-        }
-
+            };
+            leads.update(idLead, lead);
+        };
 
         $scope.removeLead = function () {
+            leads.remove(4);
+        };
 
-            leads.remove('-JO7GKm4-BIRsHI4ev9is');
-        }
-
-
-        $scope.getLeadsMonth = function () {
-
-            var month = 1;
-            var l = leads.getAllMonth(2014, 1);
-
-
-            console.log(JSON.stringify(l));
-
-        }
-    }])
-;
+//
+//        $scope.getLeadsMonth = function () {
+//
+//            var month = 1;
+//            var l = leads.getAllMonth(2014, 1);
+//            console.log(JSON.stringify(l));
+//        }
+    }]);
