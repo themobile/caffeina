c88fd6b53eaeb5798bb2e40f0b11f374c4a8fed7
Florian Cechi <florian.cechi@gmail.com>
2014-06-11 15:31:45 +0300

    da

--- mobile/www/js/controller.tests.js
+++ mobile/www/js/controller.tests.js
@@ -30,14 +30,20 @@ angular.module('caffeina.controller.tests', [])
             dmlservice.delLead($scope.idLead);
         };
 
-        $scope.setInitTemplate = function () {
+        $scope.setTemplate = function () {
             dmlservice.setInitTemplate()
         };
 
-        $scope.getRootTemplate = function () {
-            var unarray = dmlservice.getRootTemplate()
-                ;
+        $scope.addJob = function () {
+            var job = {
+                date: moment("2014-01-01").add('day', parseInt(Math.random() * 100)).format("YYYY-MM-DD"),
+                allDay: false,
+                time: "11:00",
+                type: "wedding",
+                location: "str noua bucuresti",
+                notes: "comentariuiu",
+                contact: {"name": "Florian Cechi", "phone": "7829387232", "email": "asdada@gmail.com"}
+            }
         };
-
 
     }]);
